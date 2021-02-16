c8dc9fddb3ad9a5c3ec4392e79baa311593a54e6
zaynegoering <zaynegoering@gmail.com>
2021-02-15 16:19:52 -0800

    add ghosts controller and define basic CRUD stuff

---
+++ backend/app/controllers/ghosts_controller.rb
@@ -0,0 +1,43 @@
+class GhostsController < ApplicationController
+
+    def index
+        ghosts = Ghost.all
+        render json: GhostSerializer.new(ghosts)
+    end
+
+    def show
+        ghost = Ghost.find(params[:id])
+        render json: GhostSerializer.new(ghost)
+    end
+
+    def create
+        ghost = Ghost.new(ghost_params)
+        if ghost.save
+            render json: GhostSerializer.new(ghost)
+        else
+            render json: {error: "could not save new ghost"}
+        end
+    end
+
+    def destroy
+        ghost = Ghost.find(params[:id])
+        ghost.destroy
+        render json: {message: "deleted #{ghost.name}"}
+    end
+
+    def update
+        ghost = Ghost.find(params[:id])
+        if ghost.update(ghost_params)
+            render json: GhostSerializer.new(ghost)
+        else
+            render json: {error: "could not save"}
+        end
+    end
+
+    private
+
+    def ghost_params
+        params.require(:ghost).permit(:name, :strengths, :weaknesses, :evidence)
+    end
+    
+end
