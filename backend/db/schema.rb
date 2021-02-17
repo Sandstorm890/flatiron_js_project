2f63aaa395779abc1d72f96efa04cd81c4d3b633
zaynegoering <zaynegoering@gmail.com>
2021-02-17 13:26:55 -0800

    add ghost_id to evidence table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_212513) do
+ActiveRecord::Schema.define(version: 2021_02_17_065803) do
 
   create_table "evidences", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "ghost_id"
   end
 
   create_table "ghosts", force: :cascade do |t|
