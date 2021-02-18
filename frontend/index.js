9fb431dfbfc7befebc49e6c65eea53f4fbb777c8
zaynegoering <zaynegoering@gmail.com>
2021-02-17 16:25:55 -0800

    add call to ghostApi to display ghosts

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,7 @@
+const port = `http://localhost:3000`
 const form = document.getElementById("ghost-form")
+const list = document.getElementById("ghost-list")
+const ghostApiCall = new ghostApi(port)
 const nameInput = document.getElementById("ghost-name")
 const strengthsInput = document.getElementById("ghost-strengths")
 const weaknessesInput = document.getElementById("ghost-weaknesses")
@@ -8,7 +11,6 @@ const submitButton = document.getElementById("submit-btn")
 submitButton.addEventListener('click', handleSubmit)
 
 function handleSubmit(e) {
-    debugger
     e.preventDefault()
 
     const ghostInfo = {
@@ -29,38 +31,77 @@ function handleSubmit(e) {
 
     fetch("http://localhost:3000/ghosts", configObj)
         .then(r => r.json())
+        
         .then(json => renderGhosts(json.data))
 }
 
-function renderGhosts(arg){
-    const ghosts = arg["data"]
-    ghosts.forEach(element => {
-        renderGhost(element)
-    })
-}
+ghostApiCall.getGhosts()
 
-function renderGhost(ghost){
-    const li = document.createElement('li')
-    li.dataset["id"] = ghost.id
-    li.id = `ghost-${ghost.id}`
-    li.innerHTML = `
-        <div data-id="${ghost.id}">
-            <strong class="name">${ghost.attributes.name}</strong>:
-            <span class="strengths">${item.attributes.strengths}</span> 
-            <span class="weaknesses">${ghost.attributes.weaknesses}</span>
-            <span class="evidence">${ghost.attributes.evidence}</span>
-        </div>
-        <button class="edit" data-id="${ghost.id}">Edit</button>
-        <button class="delete" data-id="${ghost.id}">Delete</button>
-    `
-    list.appendChild(li)
-    li.addEventListener('click', handleLiClick)
-}
+// function renderGhosts(arg){
+//     const ghosts = arg["data"]
+//     ghosts.forEach(element => {
+//         renderGhost(element)
+//     })
+// }
 
-function getGhosts(){
-    fetch('http://localhost:3000/ghosts')
-    .then(r => r.json())
-    .then(renderGhosts)
-}
+// function getEvidence(ghost) {
+//     // debugger
+//     return ghost.attributes.evidences.map(evidence => evidence.name)
+// }
 
-getGhosts()
+// function renderGhost(ghost){
+//     const li = document.createElement('li')
+//     li.dataset["id"] = ghost.id
+//     li.id = `ghost-${ghost.id}`
+//     li.innerHTML = `
+//         <div data-id="${ghost.id}">
+//             <strong class="name">${ghost.attributes.name}</strong>:<br>
+//             <span class="strengths">${ghost.attributes.strengths}</span><br>
+//             <span class="weaknesses">${ghost.attributes.weaknesses}</span><br>
+//             <span class="evidence">${getEvidence(ghost)}</span>
+//         </div>
+//         <button class="edit" data-id="${ghost.id}">Edit</button>
+//         <button class="delete" data-id="${ghost.id}">Delete</button>
+//     `
+    
+//     list.appendChild(li)
+//     li.addEventListener('click', handleLiClick)
+// }
+
+// function handleLiClick(e){
+//     if(e.target.innerText === "Edit"){
+
+//     } else if (e.target.innerText === "Delete"){
+//         deleteGhost(e)
+//     }
+// }
+
+// function editGhost(e) {
+    
+// }
+
+// function deleteGhost(e){
+//     e.target.parentElement.remove() // remove it before the fetch request 
+//     const id = e.target.dataset.id 
+
+//     const configObj = {
+//         method: 'DELETE',
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json"
+//         }
+//     }
+    
+//     fetch(`http://localhost:3000/ghosts/${id}`, configObj)
+//         .then(r => r.json())
+//         .then(json => alert(json.message))
+
+// }
+
+// function getGhosts(){
+//     fetch('http://localhost:3000/ghosts')
+//     .then(r => r.json())
+//     .then(renderGhosts)
+// }
+
+// getGhosts()
