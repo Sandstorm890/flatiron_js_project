85737353ca60ad72d8d7cf8a2153909080413059
zaynegoering <zaynegoering@gmail.com>
2021-02-18 18:43:19 -0800

    defined methods for drop down menu

--- frontend/src/evidence.js
+++ frontend/src/evidence.js
@@ -10,5 +10,23 @@ class Evidence {
         this.element = document.createElement("button")
         Evidence.all.push(this)
     }
+
+    addToDropDown(){
+        const option = document.createElement('option')
+        option.value  = this.id 
+        option.innerText = this.name
+        dropdown.append(option)
+    }
+
+    render() {
+        this.element.innerText = this.name 
+        this.element.id = `category-${this.id}`
+        return this.element
+    }
+
+
+    addToDom() {
+        Evidence.evidenceButtons.append(this.render())
+    }
     
 }
