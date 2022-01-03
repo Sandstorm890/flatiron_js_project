54fe2b1e8fca7cf6fb98ca1476ff73db3ec0d7c0
zaynegoering <zaynegoering@gmail.com>
2022-01-03 13:47:31 -0800

    add seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+evidences = Evidence.create([{name: "Fingerprints"}, {name: "EMF"}, {name: "Freezing Temps"}])
+ghosts = Ghost.create([{name: "Oni", weaknesses: "Stuff", strengths: "Other stuff", evidence_id: 0}, {name: "Wraith", weaknesses: "Stuff", strengths: "Other stuff", evidence_id: 1}])
